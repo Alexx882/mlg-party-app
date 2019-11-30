81f28c317f32bc81380d99d252aca85572cc8c1e
Chris <christoph.roither@gmail.com>
2019-11-30 15:33:47 +0100

    Junit tests

--- app/src/main/java/at/aau/ase/mlg_party_app/clicker/ClickerGame.java
+++ app/src/main/java/at/aau/ase/mlg_party_app/clicker/ClickerGame.java
@@ -2,7 +2,6 @@ package at.aau.ase.mlg_party_app.clicker;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.os.Vibrator;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
@@ -11,8 +10,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.security.SecureRandom;
 
 import at.aau.ase.mlg_party_app.R;
 import pl.droidsonroids.gif.GifImageView;
@@ -23,9 +20,7 @@ public class ClickerGame extends AppCompatActivity {
     private TextView tvCounter;
     private ImageView ivHitmarker;
     private GifImageView gifBackground;
-
-    private int counter;
-    private int fontsize;
+    private Logic logic;
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
@@ -37,63 +32,32 @@ public class ClickerGame extends AppCompatActivity {
         tvCounter = findViewById(R.id.tv_clicker);
         ivHitmarker = findViewById(R.id.iv_hitmarker);
         gifBackground = findViewById(R.id.gifBackground);
-        counter = 0;
-        fontsize = 20;
+        logic = new Logic();
 
 
-        ivClicker.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    tvCounter.setText(cookie_counter());
-                    textview_setup();
-                    vibrate();
-                    image_setup();
-                    image_animation();
-                    hitmarker(ivClicker.getX() + event.getX() - ivHitmarker.getWidth() / 2, ivClicker.getY() + event.getY() - ivHitmarker.getHeight() / 2);
-                }
-                if (event.getAction() == MotionEvent.ACTION_UP) {
-                    ivHitmarker.setVisibility(View.INVISIBLE);
-                }
-                return true;
+        ivClicker.setOnTouchListener((v, event) -> {
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                tvCounter.setText(logic.cookie_counter());
+                textview_setup();
+               int imageNumber = logic.image_setup();
+               if(imageNumber!=0)
+                   ivClicker.setImageResource(imageNumber);
+
+                int gifNumber = logic.gif_setup();
+                if(gifNumber!=0)
+                    gifBackground.setImageResource(gifNumber);
+
+                logic.vibrate(this);
+                image_animation();
+                hitmarker(ivClicker.getX() + event.getX() - ivHitmarker.getWidth() / 2, ivClicker.getY() + event.getY() - ivHitmarker.getHeight() / 2);
             }
+            if (event.getAction() == MotionEvent.ACTION_UP) {
+                ivHitmarker.setVisibility(View.INVISIBLE);
+            }
+            return true;
         });
     }
 
-
-    public String cookie_counter() {
-        incCounter();
-        if (counter == 69) {
-            return "Nice";
-        } else
-            return String.valueOf(getCounter());
-
-    }
-
-    public void vibrate() {
-        Vibrator click = (Vibrator) getSystemService(VIBRATOR_SERVICE);
-        click.vibrate(50);
-    }
-
-    public void image_setup() {
-        if (counter == 15) {
-            ivClicker.setImageResource(R.drawable.lvl15);
-        } else if (counter == 30) {
-            ivClicker.setImageResource(R.drawable.lvl30);
-        } else if (counter == 45) {
-            ivClicker.setImageResource(R.drawable.lvl45);
-        } else if (counter == 60)
-            ivClicker.setImageResource(R.drawable.lvl60);
-        else if (counter == 69)
-            ivClicker.setImageResource(R.drawable.lvl69);
-        else if (counter == 70)
-            ivClicker.setImageResource(R.drawable.lvl60);
-        else if (counter == 75) {
-            gifBackground.setImageResource(R.drawable.asci);
-            ivClicker.setImageResource(R.drawable.endboss);
-        }
-
-    }
 
     public void image_animation() {
         ScaleAnimation animationBig = new ScaleAnimation(1f, 0.8f, 1f, 0.8f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
@@ -102,13 +66,12 @@ public class ClickerGame extends AppCompatActivity {
     }
 
     public void textview_setup() {
-        int dg = new SecureRandom().nextInt(50) - 25;
-        tvCounter.setRotation(dg);
+        tvCounter.setRotation(logic.rand(50, 25));
         ScaleAnimation animation = new ScaleAnimation(1f, 1.2f, 1f, 1.2f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
         animation.setDuration(200);
         tvCounter.setAnimation(animation);
 
-        tvCounter.setTextSize(incsize());
+        tvCounter.setTextSize(logic.incsize());
 
 
     }
@@ -121,21 +84,7 @@ public class ClickerGame extends AppCompatActivity {
     }
 
 
-    public int getCounter() {
-        return counter;
-    }
 
-    private void incCounter() {
-        counter++;
-    }
-
-
-    public int incsize() {
-        if(fontsize>100)
-            return fontsize;
-        else
-        return fontsize++;
-    }
 
 
 }
---
+++ app/src/test/java/at/aau/ase/mlg_party_app/clicker/ClickerGameTest.java
@@ -0,0 +1,108 @@
+package at.aau.ase.mlg_party_app.clicker;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import at.aau.ase.mlg_party_app.R;
+
+public class ClickerGameTest {
+    int counter = 0;
+    Logic logic;
+
+    @Before
+    public void before() {
+        logic = new Logic();
+
+    }
+
+    @Test
+    public void counter() {
+        logic.setCounter(0);
+        Assert.assertEquals(0, logic.getCounter());
+        logic.setCounter(10);
+        Assert.assertEquals(10, logic.getCounter());
+
+    }
+
+    @Test
+    public void counter_add() {
+
+        logic.setCounter(0);
+        for (int i = 0; i < 9; i++)
+            logic.setCounter(logic.getCounter() + 1);
+
+        Assert.assertEquals(9, logic.getCounter());
+
+    }
+
+    @Test
+    public void cookie_counter(){
+        logic.setCounter(0);
+        Assert.assertEquals(1+"",logic.cookie_counter());
+
+        logic.setCounter(68);
+        Assert.assertEquals("Nice",logic.cookie_counter());
+
+    }
+
+    @Test
+    public void image_setup() {
+        logic.setCounter(0);
+        Assert.assertEquals(0, logic.image_setup());
+        logic.setCounter(15);
+        Assert.assertEquals(R.drawable.lvl15, logic.image_setup());
+        logic.setCounter(30);
+        Assert.assertEquals(R.drawable.lvl30, logic.image_setup());
+        logic.setCounter(45);
+        Assert.assertEquals(R.drawable.lvl45, logic.image_setup());
+        logic.setCounter(60);
+        Assert.assertEquals(R.drawable.lvl60, logic.image_setup());
+        logic.setCounter(69);
+        Assert.assertEquals(R.drawable.lvl69, logic.image_setup());
+        logic.setCounter(70);
+        Assert.assertEquals(R.drawable.lvl60, logic.image_setup());
+        logic.setCounter(75);
+        Assert.assertEquals(R.drawable.endboss, logic.image_setup());
+
+    }
+
+    @Test
+    public void gif_setup(){
+        logic.setCounter(0);
+        Assert.assertEquals(0,logic.gif_setup());
+
+        logic.setCounter(75);
+        Assert.assertEquals(R.drawable.asci,logic.gif_setup());
+
+    }
+
+    @Test
+    public void rand() {
+        for (int i = 0; i < 100; i++) {
+            int dg = logic.rand(10, 5);
+            Assert.assertTrue(dg >= -5 && dg < 5);
+        }
+    }
+
+    @Test
+    public void fontsize() {
+        logic.setFontsize(0);
+        Assert.assertEquals(0, logic.getFontsize());
+
+        logic.setFontsize(10);
+        Assert.assertEquals(10, logic.getFontsize());
+    }
+
+    @Test
+    public void incsize() {
+        logic.setFontsize(0);
+        for (int i = 1; i < 100; i++)
+            Assert.assertEquals(i, logic.incsize());
+
+        logic.setFontsize(99);
+        for (int i = 0; i < 100; i++)
+            Assert.assertEquals(100, logic.incsize());
+    }
+}
+
