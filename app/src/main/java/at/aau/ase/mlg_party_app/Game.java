74d441f812b4c59602c5b1e0b19980d9d88cb2e4
Alex <allercher@edu.aau.at>
2020-01-06 16:35:35 +0100

    Starting first game

--- app/src/main/java/at/aau/ase/mlg_party_app/Game.java
+++ app/src/main/java/at/aau/ase/mlg_party_app/Game.java
@@ -1,12 +1,15 @@
 package at.aau.ase.mlg_party_app;
 
 public class Game {
-    private Game(){
+
+    private Game() {
 
     }
+
     private static Game instance = null;
-    public static Game getInstance(){
-        if(instance == null)
+
+    public static Game getInstance() {
+        if (instance == null)
             instance = new Game();
         return instance;
     }
@@ -16,11 +19,24 @@ public class Game {
      */
     private String playerId;
 
+    /**
+     * The global id  of the lobby.
+     */
+    private String lobbyId;
+
     public String getPlayerId() {
         return playerId;
     }
 
     public void setPlayerId(String playerId) {
         this.playerId = playerId;
+    }
+
+    public String getLobbyId() {
+        return lobbyId;
+    }
+
+    public void setLobbyId(String lobbyId) {
+        this.lobbyId = lobbyId;
     }
 }
---
+++ app/src/main/java/at/aau/ase/mlg_party_app/networking/dtos/lobby/StartGameRequest.java
@@ -0,0 +1,9 @@
+package at.aau.ase.mlg_party_app.networking.dtos.lobby;
+
+import at.aau.ase.mlg_party_app.networking.dtos.BaseRequest;
+
+public class StartGameRequest extends BaseRequest {
+
+    public String lobbyName;
+
+}
