abc0908bee81da8620542dba312eadd26b557c49
Roither <christoph.roither@gmail.com>
2020-01-22 00:34:30 +0100

    Updatet Powermock and pulled from master

--- app/src/test/java/at/aau/ase/mlg_party_app/networking/websocket/WebSocketClientTests.java
+++ app/src/test/java/at/aau/ase/mlg_party_app/networking/websocket/WebSocketClientTests.java
@@ -1,9 +1,16 @@
 package at.aau.ase.mlg_party_app.networking.websocket;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import at.aau.ase.mlg_party_app.Callback;
 import at.aau.ase.mlg_party_app.SingletonTestsHelper;
+import at.aau.ase.mlg_party_app.networking.MessageType;
 
 public class WebSocketClientTests {
     private WebSocketClient client;
