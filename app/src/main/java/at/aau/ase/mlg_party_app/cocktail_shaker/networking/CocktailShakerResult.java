56f9d0b32d9e82f3622fb890a2a6fea2a1b69eaf
Alex <alexanderwi@edu.aau.at>
2020-01-22 17:14:06 +0100

    TicTacToe and cocktailshaker multiple rounds

--- app/src/main/java/at/aau/ase/mlg_party_app/cocktail_shaker/networking/CocktailShakerResult.java
+++ app/src/main/java/at/aau/ase/mlg_party_app/cocktail_shaker/networking/CocktailShakerResult.java
@@ -1,13 +1,15 @@
 package at.aau.ase.mlg_party_app.cocktail_shaker.networking;
 
 
+import at.aau.ase.mlg_party_app.Game;
 import at.aau.ase.mlg_party_app.networking.MessageType;
 import at.aau.ase.mlg_party_app.networking.dtos.game.GameBaseRequest;
 
 public class CocktailShakerResult extends GameBaseRequest {
-
+    public String lobbyId;
     public CocktailShakerResult(){
         type = MessageType.CocktailShakerResult;
+        this.lobbyId= Game.getInstance().getLobbyId();
     }
 
     public float max;
